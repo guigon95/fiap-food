48d940df88ce3e4faf6fa610ad090c71341900cb
unknown <guilherme_gon@live.com>
2023-07-01 11:58:24 -0300

    commit inicial

---
+++ src/main/java/br/com/project/fiapfood/FiapFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.fiapfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapFoodApplication.class, args);
+	}
+
+}
