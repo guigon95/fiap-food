41a7f259a72f0c9fffe85bde56567020e5e0a89e
unknown <guilherme_gon@live.com>
2023-07-02 22:50:09 -0300

    including object not found exception

--- src/main/java/br/com/project/fiapfood/adapters/inbound/ProductController.java
+++ src/main/java/br/com/project/fiapfood/adapters/inbound/ProductController.java
@@ -3,6 +3,7 @@ package br.com.project.fiapfood.adapters.inbound;
 import br.com.project.fiapfood.adapters.inbound.mapper.ProductMapper;
 import br.com.project.fiapfood.adapters.inbound.request.ProductRequest;
 import br.com.project.fiapfood.adapters.inbound.response.ProductResponse;
+import br.com.project.fiapfood.application.core.exception.ObjectNotFoundException;
 import br.com.project.fiapfood.application.port.in.ProductServicePort;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -71,13 +72,13 @@ public class ProductController {
             @ApiResponse(responseCode = "5xx", description = "Internal server error",
                     content = @Content) })
     public ProductResponse updateProduct(@PathVariable @Valid @org.hibernate.validator.constraints.UUID UUID id,
-                                         @RequestBody @Valid ProductRequest productRequest) throws EntityNotFoundException {
+                                         @RequestBody @Valid ProductRequest productRequest) throws ObjectNotFoundException {
 
         var product = productMapper.productRequestToProduct(productRequest);
         product.setId(id);
         var updateProduct = productServicePort.updateProduct(product);
         if (updateProduct == null){
-            throw new EntityNotFoundException();
+            throw new ObjectNotFoundException("Product not found");
         }
         return productMapper.productToProductResponse(updateProduct);
 
--- src/main/java/br/com/project/fiapfood/application/core/exception/OrderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.project.fiapfood.application.core.exception;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class OrderNotFoundException extends EntityNotFoundException {
-    public OrderNotFoundException(){
-        super();
-    }
-}
