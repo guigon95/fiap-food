781cca94d839d5e7d8ff38fd8e1df2e16fdd7bd5
ArianeAlves <ArianeAlves@gitlab.com>
2023-10-20 22:14:05 -0300

    cr

--- src/main/java/br/com/project/fiapfood/application/core/exception/InvalidFieldException.java
+++ src/main/java/br/com/project/fiapfood/application/core/exception/InvalidFieldException.java
@@ -3,7 +3,6 @@ package br.com.project.fiapfood.application.core.exception;
 import lombok.Builder;
 import lombok.Data;
 import br.com.project.fiapfood.adapter.dto.response.exceptions.ErrorMessage;
-import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
